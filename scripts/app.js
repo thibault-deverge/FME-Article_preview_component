44277a4dc72fd4de58b7523162ca7de85f62a25b
thibdvg <thibault.deverge@gmail.com>
2024-10-21 16:49:35 +0200

    First draft : Done

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,12 @@
-console.log("Hello from app.js");
+const buttons = document.querySelectorAll(".button");
+const sharePanel = document.querySelector(".share-panel");
+
+console.log(buttons);
+console.log(sharePanel);
+
+buttons.forEach((button) => {
+	button.addEventListener("click", () => {
+		sharePanel.classList.toggle("share-panel--visible");
+		console.log("click");
+	});
+});
