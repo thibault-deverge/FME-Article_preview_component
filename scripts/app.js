06b4a9798b7728d429d4e033c8e26bf6fabd6350
thibdvg <thibault.deverge@gmail.com>
2024-10-21 16:50:44 +0200

    Remove class visible and console.log

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,8 @@
 const buttons = document.querySelectorAll(".button");
 const sharePanel = document.querySelector(".share-panel");
 
-console.log(buttons);
-console.log(sharePanel);
-
 buttons.forEach((button) => {
 	button.addEventListener("click", () => {
 		sharePanel.classList.toggle("share-panel--visible");
-		console.log("click");
 	});
 });
